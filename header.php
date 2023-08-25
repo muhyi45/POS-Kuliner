5f4e07ee285717d69a2793975f50cc54265587bb
Muhyidin yahya <60137367+muhyi45@users.noreply.github.com>
2023-08-25 12:50:30 +0700

    error menu edit profile

--- header.php
+++ header.php
@@ -1,81 +1,141 @@
-<nav class="navbar navbar-expand navbar-dark bg-primary sticky-top">
-        <div class="container-lg">
-            <a class="navbar-brand" href=".">Sistem Kuliner</a>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <?php echo $hasil['username']; ?>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end mt-2">
-                            <li><a class="dropdown-item" href="#"><i class="bi bi-person-circle"></i> Profile</a></li>
-                            <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#ModalUbahPassword"><i class="bi bi-key-fill"></i> Ubah Password</a></li>
-                            <li><a class="dropdown-item" href="logout"><i class="bi bi-box-arrow-in-right"></i> Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+<?php
+include "proses/connect.php";
+$query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username='$_SESSION[username_kuliner]'");
+$records = mysqli_fetch_array($query);
+?>
 
-    <!-- Modal Edit-->
-    <div class="modal fade" id="ModalUbahPassword" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg modal-fullscreen-md-down">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Ubah Password</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+<nav class="navbar navbar-expand navbar-dark bg-primary sticky-top">
+    <div class="container-lg">
+        <a class="navbar-brand" href=".">Sistem Kuliner</a>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <?php echo $hasil['username']; ?>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end mt-2">
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#ModalUbahProfile"><i class="bi bi-person-circle"></i> Profile</a></li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#ModalUbahPassword"><i class="bi bi-key-fill"></i> Ubah Password</a></li>
+                        <li><a class="dropdown-item" href="logout"><i class="bi bi-box-arrow-in-right"></i> Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Modal Ubah Password-->
+<div class="modal fade" id="ModalUbahPassword" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-fullscreen-md-down">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Ubah Password</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form class="needs-validation" novalidate action="proses/proses_ubah_password.php" method="POST">
+
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-floating mb-3">
+                                <input disabled required type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="username" value="<?php echo $_SESSION['username_kuliner'] ?>">
+                                <label for="floatingInput">Username</label>
+                                <div class="invalid-feedback">
+                                    Masukan Email.
+                                </div>
+                            </div>
                         </div>
-                        <div class="modal-body">
-                        <form class="needs-validation" novalidate action="proses/proses_ubah_password.php" method="POST">
-                               
-                        <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="form-floating mb-3">
-                                            <input disabled required type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="username" value="<?php echo $_SESSION['username_kuliner']?>">
-                                            <label for="floatingInput">Username</label>
-                                            <div class="invalid-feedback">
-                                                Masukan Email.
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                    <div class="form-floating mb-3">
-                                            <input required type="password" class="form-control" id="floatingPassword" name="passwordlama">
-                                            <label for="floatingInput">Password Lama</label>
-                                            <div class="invalid-feedback">
-                                                Masukan Password Lama.
-                                            </div>
-                                        </div>
-                                    </div>
+                        <div class="col-lg-6">
+                            <div class="form-floating mb-3">
+                                <input required type="password" class="form-control" id="floatingPassword" name="passwordlama">
+                                <label for="floatingInput">Password Lama</label>
+                                <div class="invalid-feedback">
+                                    Masukan Password Lama.
                                 </div>
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="form-floating mb-3">
-                                            <input required type="password" class="form-control" id="floatingInput" name="passwordbaru">
-                                            <label for="floatingInput">Password Baru</label>
-                                            <div class="invalid-feedback">
-                                                Masukan Password Baru.
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                    <div class="form-floating mb-3">
-                                            <input required type="password" class="form-control" id="floatingPassword" name="repasswordbaru">
-                                            <label for="floatingInput">Ulangi Password Baru</label>
-                                            <div class="invalid-feedback">
-                                            Ulangi Password Baru.
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="submit" class="btn btn-primary" name="ubah_password_validate" value="12345">Save changes</button>
-                                </div>
-                            </form>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-floating mb-3">
+                                <input required type="password" class="form-control" id="floatingInput" name="passwordbaru">
+                                <label for="floatingInput">Password Baru</label>
+                                <div class="invalid-feedback">
+                                    Masukan Password Baru.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-floating mb-3">
+                                <input required type="password" class="form-control" id="floatingPassword" name="repasswordbaru">
+                                <label for="floatingInput">Ulangi Password Baru</label>
+                                <div class="invalid-feedback">
+                                    Ulangi Password Baru.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary" name="ubah_password_validate" value="12345">Save changes</button>
+                    </div>
+                </form>
             </div>
-            <!----- Akhir Modal Edit --- -->
+        </div>
+    </div>
+</div>
+<!----- Akhir Modal Ubah Password --- -->
+
+
+<!-- Modal Ubah Profil-->
+<div class="modal fade" id="ModalUbahProfile" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-fullscreen-md-down">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Profile</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form class="needs-validation" novalidate action="proses/proses_ubah_profile.php" method="POST">
+
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-floating mb-3">
+                                <input disabled required type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="username" value="<?php echo $_SESSION['username_kuliner'] ?>">
+                                <label for="floatingInput">Username</label>
+                                <div class="invalid-feedback">
+                                    Masukan Email.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-floating mb-3">
+                                <input required type="text" class="form-control" id="floatingNama" name="nama" value="<?php echo $records['nama'] ?>">
+                                <label for="floatingInput">Nama</label>
+                                <div class="invalid-feedback">
+                                    Masukan Nama Anda.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-floating mb-3">
+                                <input required type="number" class="form-control" id="floatingInput" name="nomor_telepon" value="<?php echo $records['nomor_telepon'] ?>">
+                                <label for="floatingInput">Nomor HP</label>
+                                <div class="invalid-feedback">
+                                    Masukan Nomor HP Anda.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary" name="ubah_profile_validate" value="12345">Save changes</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!----- Akhir Modal Ubah Profile --- -->
---
+++ proses/proses_ubah_profile.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include "connect.php";
+$nama = (isset($_POST['nama'])) ? htmlentities($_POST['nama']) : "";
+$nomor_telepon = (isset($_POST['nomor_telepon'])) ? htmlentities($_POST['nomor_telepon']) : "";
+$message = ""; 
+
+
+if(!empty($_POST['ubah_profile_validate'])){
+            $query = mysqli_query($conn, "UPDATE tb_user SET nama='$nama', nomor_telepon='$nomor_telepon' WHERE username = '$_SESSION[username_kuliner]'");
+            if(!$query){
+                $message = '<script> alert("Profile Users Gagal Diubah");
+                window.history.back()<script>';
+            }else{
+                $message = '<script> alert("Profile Users Berhasil Diubah");
+                window.history.back()<script>';
+            }
+        }else{
+                $message = '<script> alert("Terjadi Kesalahan");
+                window.history.back()<script>';
+}echo $message;
+?>
